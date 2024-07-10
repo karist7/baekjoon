2075e203f08b6e0fb0a31d8e726773e111d28cb4
karist7 <49408613+karist7@users.noreply.github.com>
2024-07-10 15:41:33 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 120 ms, Memory: 15748 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(br.readLine().length()+"\n");
+        bw.close();
+
+
+    }
+}
