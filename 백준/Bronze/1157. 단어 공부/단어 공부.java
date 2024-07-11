7ac21df530c275fa3c16139a338554f9194189fa
karist7 <49408613+karist7@users.noreply.github.com>
2024-07-11 18:39:02 +0900

    [Bronze I] Title: 단어 공부, Time: 268 ms, Memory: 21520 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -7,40 +6,22 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String str = br.readLine();
         str = str.toUpperCase();
-        Map<Character,Integer> map = new HashMap<>();
-        int t=0;
-        for(int i=0;i<str.length();i++) {
-            if (map.containsKey(str.charAt(i))) {
-                t = map.get(str.charAt(i));
-                t += 1;
-                map.put(str.charAt(i), t);
-            } else {
-                map.put(str.charAt(i), 1);
+        int[] alpha = new int[26];
+        for(int i=0;i<str.length();i++){
+            int idx = str.charAt(i)-'A';
+            alpha[idx]++;
+        }
+        int maxValue=-1;
+        char ch='?';
+        for(int i=0;i<26;i++){
+            if(maxValue<alpha[i]){
+                maxValue=alpha[i];
+                ch=(char)(i+'A');
             }
-
+            else if(alpha[i]==maxValue)
+                ch='?';
         }
-        Optional<Integer> answer = map.values().stream().max(Comparator.naturalOrder());
-        int maxNum=0;
-        if(answer.isPresent()){
-           maxNum = answer.get();
-        }
-        boolean flag = false;
-        Set<Character> keys = map.keySet();
-        char ans = 0;
-        for(char key : keys){
-            if(maxNum == map.get(key)){
-                if(ans == 0)
-                    ans = key;
-                else {
-                    flag = true;
-                    break;
-                }
-            }
-        }
-        if(flag)
-            bw.write("?"+"\n");
-        else
-            bw.write(ans+"\n");
+        bw.write(ch+"\n");
         bw.close();
     }
 
