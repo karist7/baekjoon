0628b17c623b6acda1324104d5040926e83e14d0
karist7 <49408613+karist7@users.noreply.github.com>
2024-07-16 13:02:50 +0900

    [Bronze I] Title: 진법 변환 2, Time: 112 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+        int N = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        String result = Integer.toString(N, B);
+      
+
+        bw.write(result.toUpperCase()+"\n");
+        bw.close();
+    }
+
+}
