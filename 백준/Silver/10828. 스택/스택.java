02e7e869f7a88f6aa8d44526e8a103b77f2b2066
karist7 <49408613+karist7@users.noreply.github.com>
2024-08-07 14:49:59 +0900

    [Silver IV] Title: 스택, Time: 184 ms, Memory: 19980 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        LinkedList<Integer> list = new LinkedList<>();
+        for(int i=0;i<n;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+
+            if(str.equals("push")){
+                list.add(Integer.parseInt(st.nextToken()));
+            }
+            else if(str.equals("pop")){
+                if(list.size()==0)
+                    bw.write(-1+"\n");
+                else
+                    bw.write(list.removeLast()+"\n");
+
+            }
+            else if(str.equals("top")){
+                if(list.size()==0)
+                    bw.write(-1+"\n");
+                else
+                    bw.write(list.peekLast()+"\n");
+
+            }
+            else if(str.equals("size")){
+                bw.write(list.size()+"\n");
+            }
+            else if(str.equals("empty")){
+                if(list.size()==0)
+                    bw.write(1+"\n");
+                else
+                    bw.write(0+"\n");
+            }
+
+        }
+
+        bw.close();
+    }
+}
