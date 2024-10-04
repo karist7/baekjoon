bb03d16c7c1a0d41080becec84ead081bf1757f5
karist7 <49408613+karist7@users.noreply.github.com>
2024-10-04 23:44:10 +0900

    [Silver II] Title: 최대 힙, Time: 316 ms, Memory: 26316 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=0;i<n;i++){
+            int d = Integer.parseInt(br.readLine());
+            if(d==0){
+                if(queue.isEmpty()){
+                    bw.write(0+"\n");
+                }
+                else{
+                    bw.write(Math.abs(queue.poll())+"\n");
+                }
+            }
+            else{
+                d = -d;
+                queue.add(d);
+            }
+
+        }
+
+        bw.close();
+    }
+}
