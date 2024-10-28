fa0f91b908730e27b7994d8b6561c22fdef872e4
karist7 <49408613+karist7@users.noreply.github.com>
2024-10-28 23:52:12 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<algorithm>
+
+using namespace std;
+int arr[1000001];
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	arr[0] = 0;
+	arr[1] = 0;
+	int n;
+
+	cin >> n;
+
+	for (int i = 2; i <= n; i++) {
+		arr[i] = arr[i - 1] + 1;
+		
+		if (i % 2 == 0) {
+			arr[i] = min(arr[i], arr[i / 2] + 1);
+			
+		}
+		if (i % 3 == 0) {
+			arr[i] = min(arr[i], arr[i / 3] + 1);
+			
+		}
+		
+	}
+	cout << arr[n];
+	
+	return 0;
+
+}
