3ac8a10b35af1f67f999361f7a8a0b6af219e32e
karist7 <49408613+karist7@users.noreply.github.com>
2024-10-30 00:57:58 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int arr[1001];
+	arr[1] = 1;
+	arr[2] = 2;
+	for (int i = 3; i < 1001; i++) {
+		arr[i] = (arr[i - 1] + arr[i - 2])%10007;
+	}
+
+	int n;
+	cin >> n;
+	
+	cout << arr[n];
+	
+	
+	return 0;
+
+}
