b06672578a3fe9cb8643bc3fdb6170a1c3f6122e
karist7 <49408613+karist7@users.noreply.github.com>
2024-11-29 16:56:26 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 36 ms, Memory: 5004 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.cc
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.cc
@@ -3,55 +3,72 @@
 #include <stack>
 using namespace std;
 
-static int dx[] = {0,0,-1,1};
-static int dy[] = { -1,1,0,0 };
+static int dx[] = { 0, 0, -1, 1 };
+static int dy[] = { -1, 1, 0, 0 };
 static vector<vector<bool>> visited;
 static vector<vector<char>> graph;
 static int n, m;
 static int pCount;
-void dfs(int a,int b) {
-    visited[a][b] = true;
-    for (int i = 0;i < 4;i++) {
-        int x = dx[i] + a;
-        int y = dy[i] + b;
-        if (x >= 0 && x < n && y >= 0 && y < m) {
-            if (!visited[x][y] && (graph[x][y] == 'O' || graph[x][y] == 'P')) {
-                if (graph[x][y] == 'P') {
-                    pCount++;
-                }
-                dfs(x,y);
+
+void dfs(int startX, int startY) {
+    stack<pair<int, int>> stk;  
+    stk.push({ startX, startY }); 
+
+    while (!stk.empty()) {
+        int a = stk.top().first;
+        int b = stk.top().second;
+        stk.pop(); 
+
+        // 이미 방문한 곳은 스킵
+        if (visited[a][b]) continue;  
+        visited[a][b] = true;  
+
+        // 'P'를 만나면 카운트 증가
+        if (graph[a][b] == 'P') {
+            pCount++;
+        }
+
+        // 상하좌우로 탐색
+        for (int i = 0; i < 4; i++) {
+            int x = dx[i] + a;
+            int y = dy[i] + b;
+
+            // 경계를 벗어나지 않도록 체크하고, 'X'는 스킵
+            if (x >= 0 && x < n && y >= 0 && y < m && !visited[x][y] && graph[x][y] != 'X') {
+                stk.push({ x, y });
             }
         }
     }
 }
 
 int main() {
-    
-    cin >> n >> m; 
+    cin >> n >> m;
     pCount = 0;
     graph = vector<vector<char>>(n, vector<char>(m));
     visited = vector<vector<bool>>(n, vector<bool>(m, false));
 
-    int row,column;
+    int row, column;
 
-
+    // 'I'의 위치 찾기
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            cin >> graph[i][j]; // 각 문자 입력받기
+            cin >> graph[i][j]; 
             if (graph[i][j] == 'I') {
                 row = i;
                 column = j;
             }
         }
     }
-    
+
+    // DFS 시작
     dfs(row, column);
+
+    // 'P'가 없다면 "TT" 출력, 있다면 카운트 출력
     if (pCount == 0) {
         cout << "TT";
-    }
-    else {
+    } else {
         cout << pCount;
     }
-    
+
     return 0;
 }
