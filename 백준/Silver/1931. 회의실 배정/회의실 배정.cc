88bda73aea23e1172fc884dcae7bdc77be30a77e
karist7 <49408613+karist7@users.noreply.github.com>
2024-12-01 15:12:56 +0900

    [Silver I] Title: 회의실 배정, Time: 28 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool customSort(pair<int, int> a, pair<int, int>b) {
+    if (a.second == b.second)
+        return a.first < b.first;
+    return a.second < b.second;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+    
+    vector<pair<int, int>> vec;
+   
+    
+    for (int i = 0;i < n;i++) {        
+        int a, b;
+        cin >> a >> b;
+        vec.push_back({ a,b });
+    }
+    int count = 1;
+    sort(vec.begin(), vec.end(),customSort);
+    int minTime = vec.front().first;
+    int maxTime = vec.front().second;
+    for (auto it = vec.begin()+1;it != vec.end();it++) {
+        if (maxTime <= it->first) {
+                minTime = it->first;
+                maxTime = it->second;
+                
+                count++;
+        }
+    }
+    cout << count;
+    
+
+
+    return 0;
+}
