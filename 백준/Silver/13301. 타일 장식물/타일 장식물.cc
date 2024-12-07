9797f8a0bcbb97b68ab46aba2877f65d8f406176
karist7 <49408613+karist7@users.noreply.github.com>
2024-12-07 15:22:28 +0900

    [Silver V] Title: 타일 장식물, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13301. 타일 장식물/타일 장식물.cc
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.cc
@@ -4,22 +4,48 @@
 
 using namespace std;
 
+string fibonacci(string a, string b) {
+    string result = "";
+    int sum=0,carry = 0;
 
+    while (a.length() > b.length()) {
+        b = '0' + b;
+    }
+    for (int i = a.length() - 1;i >= 0;i--) {
+        sum = (a[i] - '0') + (b[i] - '0') + carry;
+        carry = sum / 10;
+        result = char(sum%10 + '0') + result;
+    }
+    if (carry > 0)result = '1' + result;
+    return result;
+}
+string mul(string c) {
+    string result = "";
+    int total = 0, carry = 0;
+    for (int i = c.length() - 1;i >= 0;i--) {
+        total = ((c[i] - '0') * 2) + carry;
+        carry = total / 10;
+        result = char(total % 10 + '0') + result;
+    }
+    if (carry > 0)result = '1' + result;
+    return result;
+}
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     int n;
     cin >> n;
-    long long fibo[81];
-    fibo[0] = 0;
-    fibo[1] = 1;
+    string fibo[81];
+    fibo[0] = "0";
+    fibo[1] = "1";
     for (int i = 2;i <= n;i++) {
-        fibo[i] = fibo[i - 1]+ fibo[i - 2];
+        fibo[i] = fibonacci(fibo[i - 1], fibo[i - 2]);
     }
     
+    string plus = fibonacci(fibonacci(fibo[n], fibo[n - 1]),fibo[n] );
     
-    int circumference = (fibo[n] + (fibo[n] + fibo[n - 1])) * 2;
-    cout << circumference;
+    string result = mul(plus);
+    cout << result;
     return 0;
 }
