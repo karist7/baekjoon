da04fb226cf38f6966cd4e4c04ae1e0d65d08f99
karist7 <49408613+karist7@users.noreply.github.com>
2026-03-13 18:03:56 +0900

    [Bronze II] Title: 단어의 개수, Time: 4 ms, Memory: 3680 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -1,36 +1,21 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <string>
-#include <stack>
-#include <queue>
-
 using namespace std;
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	string str;
-	getline(cin,str);
-	int a = 0;
-	int sol = 0;
-	
-	if (!str.empty() && str.at(0)==' ' ) {
-		str.erase(0, 1);
-	}
-	if (!str.empty() && str.at(str.length() - 1) == ' ' ) {
-		str.erase(str.length() - 1, str.length());
-	}
-	for (int i = 0; i < str.length(); i++) {
-		if (str[i] == ' ') {
-			sol++;
-		}
-	}
-	if (str.length() == 0) {
-		cout << sol;
-	}
-	else {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-		cout << sol + 1;
-	}
+    string str;
+    getline(cin, str);
+
+    int sol = 0;
+
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] != ' ' && (i == 0 || str[i - 1] == ' ')) {
+            sol++;
+        }
+    }
+
+    cout << sol;
 }
