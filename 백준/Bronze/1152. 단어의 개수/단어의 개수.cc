91796217f26edbe6b95444b22d9e8c5a89f153f5
karist7 <49408613+karist7@users.noreply.github.com>
2026-03-13 17:34:21 +0900

    [Bronze II] Title: 단어의 개수, Time: 8 ms, Memory: 3680 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	string str;
+	getline(cin,str);
+	int a = 0;
+	int sol = 0;
+	
+	
+	if (!str.empty() && str.at(0)==' ' ) {
+		str.erase(0, 1);
+	}
+	if (!str.empty() && str.at(str.length() - 1) == ' ' ) {
+		str.erase(str.length() - 1, str.length());
+	}
+	while (str.find(" ", a) != string::npos) {
+		a = str.find(" ", a) + 1;
+		sol++;
+	}
+	if (str.length() == 0) {
+		cout << sol;
+	}
+	else {
+
+		cout << sol+1;
+	}
+	
+}
