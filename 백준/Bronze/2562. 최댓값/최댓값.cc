30c24293663e37238d4d5d5c5380ad6557e9b465
karist7 <49408613+karist7@users.noreply.github.com>
2026-03-13 18:23:28 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int maxVal = INT32_MIN;
+	int idx = 0;
+	int a;
+	vector<int> arr(9);
+	for (int i = 0; i < 9; i++) {
+		cin >> a;
+		if (maxVal < a) {
+			maxVal = a;
+			idx = i;
+		}
+
+	}
+	cout << maxVal << "\n" << idx+1;
+
+	
+}
