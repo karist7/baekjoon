6ce9189e68b91e7a9caecf357b28a4512925dc18
karist7 <49408613+karist7@users.noreply.github.com>
2026-03-31 01:28:22 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+
+
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, m;
+    cin >> n>>m;
+    vector<int> arr;
+    for (int i = 0; i < n; i++) {
+        int k;
+        cin >> k;
+        arr.push_back(k);
+    }
+    int maxVal = 0;
+    int subVal;
+    bool correct = false;
+    for (int i = 0; i < arr.size() - 2; i++) {
+        for (int j = i+1; j < arr.size() - 1; j++) {
+            for (int l = j+1; l < arr.size(); l++) {
+                subVal = arr[i] + arr[j] + arr[l];
+                if (maxVal == m) {
+                    correct = true;
+                    break;
+                }
+                if (maxVal < subVal && subVal <= m) maxVal = subVal;
+
+            }
+            if (correct) break;
+        }
+        if (correct) break;
+    }
+    cout << maxVal;
+    
+}
